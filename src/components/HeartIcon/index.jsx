754ff410de0d26adc6bd030982a45e8ee4da87d8
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2023-05-04 15:09:41 -0300

    fix: Heart and Ticket icons path

--- src/components/HeartIcon/index.jsx
+++ src/components/HeartIcon/index.jsx
@@ -1,5 +1,7 @@
+import pixelHeart from "../../assets/icons/pixelHeart.png"
+
 export const HeartIcon = () => {
     return (
-        <img style={{ filter: "drop-shadow(0 0 3px #C2C2C2)" }} className="col-2" src={"./src/assets/icons/pixelHeart.png"} alt="Heart Icon" />
+        <img style={{ filter: "drop-shadow(0 0 3px #C2C2C2)" }} className="col-2" src={pixelHeart} alt="Heart Icon" />
     )
 }
--- src/components/TicketIcon/index.jsx
+++ src/components/TicketIcon/index.jsx
@@ -1,5 +1,7 @@
+import pixelTicket from "../../assets/icons/pixelTicket.png"
+
 export const TicketIcon = () => {
     return (
-        <img style={{ filter: "drop-shadow(0 0 3px #C2C2C2)" }} className="col-2" src={"./src/assets/icons/pixelTicket.png"} alt="Heart Icon" />
+        <img style={{ filter: "drop-shadow(0 0 3px #C2C2C2)" }} className="col-2" src={pixelTicket} alt="Heart Icon" />
     )
 }
