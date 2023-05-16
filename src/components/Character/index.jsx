5cce1c453f3ba74618a12b3c952ec924f71f977c
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2023-05-16 17:04:21 -0300

    refactor: Character container text and image size

--- src/components/Character/index.jsx
+++ src/components/Character/index.jsx
@@ -2,8 +2,14 @@ import style from "./style.module.css";
 
 export const Character = ({ character }) => {
     return (
-        <div className={`bg-white rounded-4 shadow d-flex justify-content-center align-items-center ${style.characterContainer}`}>
-            <img className={style.characterPhoto} src={character.photo} alt="characterPhoto" />
+        <div className={`bg-white rounded-4 shadow ${style.characterContainer}`}>
+            <figure className={style.characterFigure}>
+                <img src={character.photo} alt="characterPhoto" />
+                <figcaption>
+                    <p className="text-center">{character.name}</p>
+                    <p className={style.context}>{character.context}</p>
+                </figcaption>
+            </figure>
         </div>
     )
 }
